d2a50bfe6561e675eb29809ed168856bc460b350
arshitmodi <69080166+arshitmodi@users.noreply.github.com>
2020-10-27 01:27:22 +0530

    Update traverseParseTree.c

--- traverseParseTree.c
+++ traverseParseTree.c
@@ -1,4 +1,13 @@
-#include "Tree.H"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "grammar.h"
+#include "lexer.h"
+#include "stack.h"
+#include "Tree.h"
+#include "parser.h"
+
 void declaration_statement(TreeNode* root, TypeExpTable t)
 {
     TreeNode* temp=root->rightChild->leftSib;
