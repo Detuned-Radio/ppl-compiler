d1e7ccc6b3d5446abcb77edf8a5cc04280c06a08
devanshu-bits <69413997+devanshu-bits@users.noreply.github.com>
2020-10-29 17:38:43 +0530

    Update traverseParseTree.c

--- traverseParseTree.c
+++ traverseParseTree.c
@@ -189,6 +189,7 @@ void processJagg2DDecStmt(TreeNode * jaggDecStmt){
     x++;
      if(flag == false)
       printError(init->leftChild,false,NULL,NULL,NULL,init->leftChild -> depth,"type definition error");
+    flag=true;
   }while(init->leftChild != init->rightChild);
 }
 
@@ -254,7 +255,7 @@ void processJagg3DDecStmt(TreeNode * jaggDecStmt){
     {
       printError(init->leftChild,false,NULL,NULL,NULL,init->leftChild -> depth,"type definition error");
     }
-    
+    flag=true;
     x++;
     init = init->rightChild;
   }while(pass1);
